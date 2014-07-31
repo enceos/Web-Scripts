f73e92bd9d9cd66cbc39792a836ca5a8d449f45f
enceos <blizard2002@gmail.com>
2014-07-31 22:24:57 +0600

    Scroll distance update

--- smoothPageScroll.js
+++ smoothPageScroll.js
@@ -5,7 +5,7 @@ $(function(){
 	var $window = $(window);		//Window object
 	
 	var scrollTime = 0.3;			//Scroll time
-	var scrollDistance = 50;		//Distance. Use smaller value for shorter scroll and greater value for longer scroll
+	var scrollDistance = 150;		//Distance. Use smaller value for shorter scroll and greater value for longer scroll
 	
 	$window.on("mousewheel DOMMouseScroll", function(event){
 		
